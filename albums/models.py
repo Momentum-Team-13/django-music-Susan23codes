458086b21beeb04496d2edcca53bcd6eabf34eca
Susan <susan23codes@gmail.com>
2022-07-04 20:41:17 -0400

    Migrated Artist model

--- albums/models.py
+++ albums/models.py
@@ -1,12 +1,17 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+
 class Album(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     created_date = models.DateTimeField(auto_now_add=True)
     is_favorite = models.BooleanField(default=False)
     album_art_url = models.CharField(max_length=1023, blank=True, null=True)
+    artist_other = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="albums", blank=True, null=True)
 
     def __str__(self) -> str:
         return self.title
